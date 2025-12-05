bdaff9f3206a76f1284e91a3aca262412c63ea66
Henry Luong <tut56598@temple.edu>
2025-12-05 11:39:50 -0500

    player movement

--- main.py
+++ main.py
@@ -4,15 +4,43 @@ import pygame
 
 WIDTH = 800
 HEIGHT = 800
+FRAMES = 60
 running = True
 
+clock = pygame.time.Clock()
 screen = pygame.display.set_mode(((WIDTH, HEIGHT)))
-screen.fill((255, 255, 255))  # background (plain color, note: how to set as image?)
+# screen.fill((255, 255, 255))  # background (plain color, note: how to set as image?)
 
 pygame.display.set_caption("hello world")
-pygame.display.flip()
+
+playerWidth = 50
+playerHeight = 50
+playerX = WIDTH // 2 # initial x pos
+playerY = HEIGHT - 100 # initial y pos
+playerSpeed = 5
 
 while running == True:
+    keys = pygame.key.get_pressed()
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            
+    if keys[pygame.K_w]:
+        playerY -= playerSpeed
+    if keys[pygame.K_s]:
+        playerY += playerSpeed
+    if keys[pygame.K_a]:
+        playerX -= playerSpeed
+    if keys[pygame.K_d]:
+        playerX += playerSpeed
+
+    # make sure player stay inside the window
+    playerX = max(0, min(WIDTH - playerWidth, playerX))
+    playerY = max(0, min(HEIGHT - playerHeight, playerY))
+
+    screen.fill((255, 255, 255)) # IMPORTANT, redraws / update background???
+    pygame.draw.rect(screen, (0, 0, 255), (playerX, playerY, playerWidth, playerHeight))
+
+    pygame.display.flip()
+    clock.tick(FRAMES)
