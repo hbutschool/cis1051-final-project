c366ca54446700a5e035e47b17ddb8a4ba8d6e8b
Henry Luong <tut56598@temple.edu>
2025-12-05 11:53:54 -0500

    boss 1 moves left and right

--- main.py
+++ main.py
@@ -19,13 +19,20 @@ playerX = WIDTH // 2 # initial x pos
 playerY = HEIGHT - 100 # initial y pos
 playerSpeed = 5
 
+bossWidth = 80
+bossHeight = 80
+bossX =(WIDTH - bossWidth) // 2
+bossY = 100
+bossSpeed = 10
+bossDirection = 1 # 1 = right, -1 = left
+
 while running == True:
     keys = pygame.key.get_pressed()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-            
+
     if keys[pygame.K_w]:
         playerY -= playerSpeed
     if keys[pygame.K_s]:
@@ -35,12 +42,18 @@ while running == True:
     if keys[pygame.K_d]:
         playerX += playerSpeed
 
+    bossX += bossSpeed * bossDirection
+    
+    if (bossX <= 0) or (bossX + bossWidth >= WIDTH):
+            bossDirection *= -1
+
     # make sure player stay inside the window
     playerX = max(0, min(WIDTH - playerWidth, playerX))
     playerY = max(0, min(HEIGHT - playerHeight, playerY))
 
     screen.fill((255, 255, 255)) # IMPORTANT, redraws / update background???
     pygame.draw.rect(screen, (0, 0, 255), (playerX, playerY, playerWidth, playerHeight))
+    pygame.draw.rect(screen, (255, 0, 0), (bossX, bossY, bossWidth, bossHeight))
 
     pygame.display.flip()
     clock.tick(FRAMES)
